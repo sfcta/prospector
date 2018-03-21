63ab1fe4f4ce1815fa062f2ab1a3c0c26a33056d
John Rumpel <jrumpel@sfcta.org>
2018-03-21 09:54:58 -0700

    with 2017 added provisionally, changed html

--- prospector/src/safety/code.js
+++ prospector/src/safety/code.js
@@ -26,7 +26,6 @@ let missingColor = '#ccc';
 let popup = null;
 let collisionLayer;
 let mapLegend;
-let years = [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016];
 let allJSONData;
 
 //Initialization of selective aspects
