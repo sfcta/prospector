eab33d0a265ec2a6f0eb4ecfe0e87cb7e8936704
bhargavasana <bhargavs_in@yahoo.com>
2019-03-28 16:48:56 -0700

    modify ordering of additional context layers

--- prospector-frontend/src/csf_jobpop/code.js
+++ prospector-frontend/src/csf_jobpop/code.js
@@ -55,20 +55,20 @@ let stripes = new L.StripePattern({weight:3,spaceWeight:3,opacity:0.6,angle:135}
 
 const ADDLAYERS = [
   {
-    view: 'sup_district_boundaries', name: 'Supervisorial District Boundaries',
-    style: { opacity: 1, weight: 3, color: '#730073', fillOpacity: 0, interactive: false},
-  },
-  {
     view: 'coc2017_diss', name: 'Communities of Concern',
     style: { opacity: 1, weight: 2, color: 'grey', fillPattern: stripes, interactive: false},
+  },
+  {
+    view: 'hin2017', name: 'High Injury Network',
+    style: { opacity: 1, weight: 3, color: '#FF8C00', interactive: false},
   },
   {
     view: 'sfparks', name: 'Major Parks',
     style: { opacity: 1, weight: 2, color: 'grey', fillPattern: stripes, interactive: false},
   },
   {
-    view: 'hin2017', name: 'High Injury Network',
-    style: { opacity: 1, weight: 3, color: '#FF8C00', interactive: false},
+    view: 'sup_district_boundaries', name: 'Supervisorial District Boundaries',
+    style: { opacity: 1, weight: 3, color: '#730073', fillOpacity: 0, interactive: false},
   },
 ]
 
