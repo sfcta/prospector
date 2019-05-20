546191017fb9723e6ad372237edf70b0e0c53832
bhargavasana <bhargavs_in@yahoo.com>
2019-05-20 16:44:34 -0700

    help and feedback submission text edits

--- prospector-frontend/src/csf_jobpop/code.js
+++ prospector-frontend/src/csf_jobpop/code.js
@@ -697,7 +697,7 @@ function onCaptchaVerified(recaptchaToken) {
       app.submit_loading = false;
     } else {
       comment.select_year = app.selected_year;
-      comment.select_mode = app.selected_metric;
+      comment.select_metric = app.selected_metric;
       comment.add_layer = app.addLayers;
       comment.comment_user = getCookie("username");
       comment.comment_time = timestamp;
@@ -709,7 +709,7 @@ function onCaptchaVerified(recaptchaToken) {
       }
       //console.log(JSON.stringify(comment));
       postComments(comment);
-      app.comment_instruction = 'Thank you for your feedback! You can provide more.';
+      app.comment_instruction = 'Thank you for your feedback!';
       app.comment = '';
       app.submit_loading = false;
       // app.submit_disabled = true;
