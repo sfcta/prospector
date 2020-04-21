cb566a81ac12b35e2d9da1363416ae260b2083f1
bhargavasana <bhargavs_in@yahoo.com>
2020-04-21 11:41:48 -0700

    cmprt: sort by date for logitudinal charts

--- prospector-frontend/src/cmprt/code.js
+++ prospector-frontend/src/cmprt/code.js
@@ -171,7 +171,7 @@ async function parseAllAggregateData(buildAggData, jsonData, viz) {
 
   // Push AM data
   let data = [];
-  for (let date in byYearAM) {
+  for (let date of Object.keys(byYearAM).sort()) {
     data.push({
       date: date,
       art: byYearAM[date]['Arterial'],
@@ -182,7 +182,7 @@ async function parseAllAggregateData(buildAggData, jsonData, viz) {
 
   // Push PM data
   data = [];
-  for (let date in byYearPM) {
+  for (let date of Object.keys(byYearPM).sort()) {
     data.push({
       date: date,
       art: byYearPM[date]['Arterial'],
@@ -407,7 +407,7 @@ function buildChartHtmlFromCmpData(json = null) {
       if (!byYear[entry.date]) byYear[entry.date] = {};
       byYear[entry.date][entry.period] = val;
     }
-    for (let date in byYear) {
+    for (let date of Object.keys(byYear).sort()) {
       if (app.isAMActive) {
         data.push({date: date, period: byYear[date]['AM']});
       } else {
